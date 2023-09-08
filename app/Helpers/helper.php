fa257a07a1339379420503e539ef509b764db134
maulana677 <maull0776@gmail.com>
2023-09-08 09:03:21 +0700

    adding K format to view count

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Language;
+use PhpParser\Node\Expr\Cast\String_;
 
 /** format tag berita */
 function formatTags(array $tags): String
@@ -37,4 +38,16 @@ function setLanguage(string $code): void
 function truncate(string $text, int $limit = 50): String
 {
     return \Str::limit($text, $limit, '...');
+}
+
+/** convert a number in K format */
+function convertToKFormat(int $number): String
+{
+    if ($number < 1000) {
+        return $number;
+    } elseif ($number < 1000000) {
+        return round($number / 1000, 1) . 'K';
+    } else {
+        return round($number / 1000000, 1) . 'M';
+    }
 }
