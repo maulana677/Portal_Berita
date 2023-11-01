3449cdbcafeeda58564a8198b194c6979e225e1c
maulana677 <maull0776@gmail.com>
2023-11-01 21:21:47 +0700

    Bahasa Dinamis - Menerjemahkan Nilai Bahasa dengan Microsoft Trans. API part 2

--- app/Http/Controllers/Admin/LocalizationController.php
+++ app/Http/Controllers/Admin/LocalizationController.php
@@ -79,6 +79,21 @@ class LocalizationController extends Controller
 
     public function translateString(Request $request)
     {
-        dd($request->all());
+        $client = new \GuzzleHttp\Client();
+
+        $response = $client->request('POST', 'https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=de&api-version=3.0&profanityAction=NoAction&textType=plain', [
+            'body' => '[
+            {
+                "Text": "I would really like to drive your car around the block a few times."
+            }
+        ]',
+            'headers' => [
+                'X-RapidAPI-Host' => 'microsoft-translator-text.p.rapidapi.com',
+                'X-RapidAPI-Key' => 'dbd8d4e7c5msh4f0615717a94277p169889jsndbfd909766cc',
+                'content-type' => 'application/json',
+            ],
+        ]);
+
+        echo $response->getBody();
     }
 }
