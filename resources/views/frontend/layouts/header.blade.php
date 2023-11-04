3e4e3744df5f663bfc155ab39ceeb1bb37d8433c
maulana677 <maull0776@gmail.com>
2023-11-04 10:58:19 +0700

    Navbar Header - Ubah route part 2

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -66,7 +66,7 @@
                 <div class="collapse navbar-collapse justify-content-between" id="main_nav99">
                     <ul class="navbar-nav ml-auto ">
                         <li class="nav-item">
-                            <a class="nav-link active" href="index.html">home</a>
+                            <a class="nav-link active" href="{{ url('/') }}">home</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link" href="{{ route('about') }}"> about </a>
