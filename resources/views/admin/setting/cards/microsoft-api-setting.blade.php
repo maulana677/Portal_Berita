679bb20fbb866aa3561eafa795c4d6fee9717eb0
maulana677 <maull0776@gmail.com>
2024-03-05 22:34:22 +0700

    fix update setting cards

--- resources/views/admin/setting/cards/microsoft-api-setting.blade.php
+++ resources/views/admin/setting/cards/microsoft-api-setting.blade.php
@@ -6,7 +6,8 @@
             <div class="form-group">
                 <label>{{ __('admin.Microsoft Api Host') }}</label>
                 <div class="input-group">
-                    <input value="" name="site_microsoft_api_host" type="text" class="form-control">
+                    <input value="{{ $settings['site_microsoft_api_host'] }}" name="site_microsoft_api_host" type="text"
+                        class="form-control">
                     @error('site_microsoft_api_host')
                         <p class="text-danger">{{ $message }}</p>
                     @enderror
@@ -15,7 +16,8 @@
             <div class="form-group">
                 <label>{{ __('admin.Microsoft Api Key') }}</label>
                 <div class="input-group">
-                    <input value="" name="site_microsoft_api_key" type="text" class="form-control">
+                    <input value="{{ $settings['site_microsoft_api_key'] }}" name="site_microsoft_api_key"
+                        type="text" class="form-control">
                     @error('site_microsoft_api_key')
                         <p class="text-danger">{{ $message }}</p>
                     @enderror
